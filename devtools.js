1ff7f5b17e5aab5e6e54226c6484a7c36ab99709
Siphon880gh <weng.f.fung@gmail.com>
2023-10-02 19:17:12 -0700

    DevTools Subpanel of Element

--- devtools.js
+++ devtools.js
@@ -1,6 +1,6 @@
-let panel
+let panel, sidebar = null
 
-chrome.devtools.panels.create("Element Designer", "icon.png", "panel.html", panel => {
+chrome.devtools.panels.create("Tailwind-Bootcamp Templates", "icon.png", "panel.html", panel => {
     panel = panel;
     // code invoked on panel creation
     panel.onShown.addListener((extPanelWindow) => {
@@ -8,3 +8,30 @@ chrome.devtools.panels.create("Element Designer", "icon.png", "panel.html", pane
     });
 });
 
+// Background page
+
+chrome.devtools.panels.elements.createSidebarPane("Templates", 
+  function(sidebar) {
+    sidebar = sidebar;
+
+  sidebar.onShown.addListener(function() {
+    // sidebar.setObject({someObject: true});
+    // Panel content
+    panel.setPage('popup.html')
+    panel.setHeight('100px');
+
+    sidebar.setPage('popup.html')
+    sidebar.setHeight('100px');  
+  });
+
+});
+
+// Listen for selection changes
+chrome.devtools.panels.onSelectionChanged.addListener(() => {
+  
+  // Get selected element
+  chrome.devtools.inspectedWindow.eval("$", (element) => {
+    sidebar.setObject({selectedElement: element});; 
+  });
+
+});
