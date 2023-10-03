b15094f73012086d3a06f954ef3933adf27c8035
Siphon880gh <weng.f.fung@gmail.com>
2023-10-03 15:27:39 -0700

    Can swap outerHTML or innerHTML 1 of 2

--- popup.js
+++ popup.js
@@ -1,5 +1,44 @@
-/**
- * App is singleton object that contains the important peices
- */
 var app = {
+    store: null,
+    init: (store)=>{
+        this.store = store;
+    },
+    observe: {
+        DOMContentReady:()=>{
+            document.querySelectorAll('[name="htmlLevel"]').forEach(inputRadio=>{
+               inputRadio.addEventListener('change', function(ev){
+                    switch(ev.target.value) {
+                        case "outerHTML":
+                            app.setState.setOuterHTML();
+                            break;
+                        case "innerHTML":
+                            app.setState.setInnerHTML();
+                            break;
+                    }
+                }); // inputRadio on change
+            }); // all input radios
+        } // DOMContentReady
+    }, // observe
+    setState: {
+        setOuterHTML: ()=>{
+            this.store.set({swapMode: 'outerHTML'}, function() {
+                // alert('Value is set to ' + 'outerHTML');
+            });
+        },
+        setInnerHTML: ()=>{
+            this.store.set({swapMode: 'innerHTML'}, function() {
+                // alert('Value is set to ' + 'innerHTML');
+            });
+        }
+    },
+    getState: {
+        getSwapMode: ()=>{
+            this.store.get('swapMode', function(result) {
+                // alert('Value currently is ' + result.swapMode);
+            });
+        }
+    }
 }
+
+app.init(chrome.storage.local);
+app.observe.DOMContentReady();
--- sidebar.js
+++ sidebar.js
@@ -4,6 +4,9 @@ document.querySelector("#chose-bootstrap").addEventListener("click", function() 
     console.log("Swapping to:")
     console.log(swapWith)
     
+    let swapMode = app.getState.getSwapMode();
+    if(!swapMode) swapMode = "outerHTML"; // default
+
     /* Will abandon for a background.js approach */
-    chrome.runtime.sendMessage({type:"swapHTML", data: swapWith})
+    chrome.runtime.sendMessage({type:"swapHTML", data: swapWith, swapMode})
 });
