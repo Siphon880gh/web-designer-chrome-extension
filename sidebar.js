697850fc5270ea92b40040f56703ab5b458ef5b9
Siphon880gh <weng.f.fung@gmail.com>
2023-10-03 18:44:00 -0700

    User hint for tabs

--- sidebar.js
+++ sidebar.js
@@ -2,24 +2,53 @@ const buttons = document.querySelectorAll('button.where-to');
 buttons.forEach(button => {
   button.addEventListener('click', (ev) => {
     buttons.forEach(btn => btn.classList.remove('active'));
-    ev.target.classList.add('active');
+
+    let currentTab = ev.target;
+    if(!currentTab.matches("button")) {
+        currentTab = currentTab.closest("button");
+    }
+    currentTab.classList.add('active');
   });
 });
 
-document.querySelector("#chose-bootstrap").addEventListener("click", async function() {
-    let swapWith = "<div>Bootstrap</div>";
 
-    console.log("Swapping to:")
-    console.log(swapWith)
-    
-    let swapModeWrapper = await chrome.storage.local.get('swapMode');
-    let swapMode = swapModeWrapper?.swapMode;
-    if(!swapMode) swapMode = "outerHTML"; // default
+var tooltipHovers = document.querySelectorAll("[data-tooltip-target]");
+tooltipHovers.forEach(tooltipHover=>{
+    tooltipHover.addEventListener("mouseenter", function(ev) {
+        var id = ev.target.getAttribute("data-tooltip-target");
+        document.querySelector(`[data-tooltip-id=${id}]`).classList.add("active");
+        console.log("hovered");
+      });
+    tooltipHover.addEventListener("mouseleave", function(ev) {
+        var id = ev.target.getAttribute("data-tooltip-target");
+        document.querySelector(`[data-tooltip-id=${id}]`).classList.remove("active");
+        console.log("hovered");
+    });
+})
+var tooltipCloses = document.querySelectorAll("[[data-tooltip-id]");
+tooltipCloses.forEach(tooltipClose=>{
+    tooltipClose.addEventListener("click", function(ev) {
+        ev.target.classList.remove("active")
+    });
+})
 
-    /* Will abandon for a background.js approach */
-    // swapMode is innerHTML or outerHTML
-    // insertMode is swapping element, prepending body, or appending body
-    let insertMode = document.querySelector('button.active').dataset.value;
-    console.log({insertMode})
-    chrome.runtime.sendMessage({type:"swapHTML", data: swapWith, swapMode, insertMode})
-});
+
+document.querySelectorAll(".chose-bootstrap").forEach(chose=>{
+        chose.addEventListener("click", async function() {
+            let swapWith = "<div>Bootstrap</div>";
+
+            console.log("Swapping to:")
+            console.log(swapWith)
+            
+            let swapModeWrapper = await chrome.storage.local.get('swapMode');
+            let swapMode = swapModeWrapper?.swapMode;
+            if(!swapMode) swapMode = "outerHTML"; // default
+
+            /* Will abandon for a background.js approach */
+            // swapMode is innerHTML or outerHTML
+            // insertMode is swapping element, prepending body, or appending body
+            let insertMode = document.querySelector('button.active').dataset.value;
+            console.log({insertMode})
+            chrome.runtime.sendMessage({type:"swapHTML", data: swapWith, swapMode, insertMode})
+    })
+})
