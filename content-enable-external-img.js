2726cc148982902a731ea40f74814caefceb3614
Siphon880gh <weng.f.fung@gmail.com>
2023-10-03 22:08:51 -0700

    Content inject to allow external image loading on more websites

---
+++ content-enable-external-img.js
@@ -0,0 +1,13 @@
+console.log("content-enable-external-img running")
+
+/** Doesn't work on all websites. 
+ * Apple.com still blocks images from your templates from loading, whereas this is effective on bmwusa.com 
+ * There's no workaround. */
+const metaTags = document.getElementsByTagName('meta');
+for (let i = 0; i < metaTags.length; i++) {
+  if (metaTags[i].getAttribute('http-equiv') === 'Content-Security-Policy') {
+    // Modify the CSP content attribute to allow external images
+    const cspContent = metaTags[i].getAttribute('content');
+    metaTags[i].setAttribute('content', cspContent + " img-src *;");
+  }
+}
